adee65c303339573c1eeef432e3ead69ee1687b8
Darius San Agustin <darius.sanagustin116@gmail.com>
2019-12-16 14:11:18 -0500

    modified test_mastodon.py to take a string from CLI to post to Mastodon.

--- src/test_mastodon.py
+++ src/test_mastodon.py
@@ -1,18 +1,28 @@
+from argparse import ArgumentParser
 from mastodon import Mastodon
 import os
 
 """Simple test script that posts something from a test account"""
 
 
-# imports access token and api base url from environment
-mastodon = Mastodon(
-	access_token=os.environ['MASTODON_ACCESS_TOKEN'],
-	api_base_url=os.environ['MASTODON_BASE_URL']
-)
+def create_and_log_in():
+	# imports access token and api base url from environment
+	mastodon = Mastodon(
+		access_token=os.environ['MASTODON_ACCESS_TOKEN'],
+		api_base_url=os.environ['MASTODON_BASE_URL']
+	)
 
-mastodon.log_in(
-	'test@gmail.com',
-	'asdf1234'
-)
+	mastodon.log_in(
+		'test@gmail.com',
+		'asdf1234'
+	)
 
-mastodon.toot("Hello guys, welcome to my Minecraft let's play!")
+	return mastodon
+
+if __name__ == '__main__':
+	parser = ArgumentParser(description='Simple Test CLI tool that toots in mastodon')
+	parser.add_argument("-p", type=str, help="status you want to toot", required=True)
+	args = parser.parse_args()
+
+	mastodon = create_and_log_in()
+	mastodon.toot(args.p)
